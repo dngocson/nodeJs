8ed4ed8fd226f3ff8f20b3f6778a6a62c0575a53
dngocson <dngocson12@gmail.com>
2023-11-06 20:41:06 +0700

    section 4

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const http = require("http");
 const express = require("express");
+const morgan = require("morgan");
+
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
-const server = http.createServer(app);
-server.listen(3000);
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+module.exports = app;
